d629c4e24156ad9ce3f5447620b86682757f2ca9
刘威 <liuwei89@jd.com>
2022-01-07 15:33:12 +0800

    feat: 对比新旧节点做增/删/改 事件的注册和取消

--- src/index.js
+++ src/index.js
@@ -20,19 +20,76 @@ function createTextElement(text) {
   };
 }
 
+function createDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type);
+  updateDom(dom, {}, fiber.props);
+  return dom;
+}
+const isEvent = key => key.startsWith('on');
+const isProperty = key => key !== 'children' && !isEvent(key);
+const isNew = (prev, next) => key => prev[key] !== next[key];
+const isGone = (prev, next) => key => !(key in next);
+
+function updateDom(dom, prevProps, nextProps) {
+  // 移除或者修改监听事件
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+      key =>
+        // 移除的监听事件
+        !(key in nextProps) ||
+        // 监听事件改变了
+        isNew(prevProps, nextProps)(key),
+    )
+    .forEach(name => {
+      const eventType = name.toLocaleLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+  // 移除属性
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = '';
+    });
+  // 增加或者修改属性
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew)
+    .forEach(name => {
+      dom[name] = nextProps[name];
+    });
+  // 新增监听事件
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name.toLocaleLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
 function commitRoot() {
+  deletions.forEach(commitWork);
   // 此处必须是渲染子元素 因为第一个fiber是根据容器dom构造出来的
   commitWork(wipRoot.child);
-  currentRoot = wipRoot
+  currentRoot = wipRoot;
   wipRoot = null;
 }
 
 function commitWork(fiber) {
-  if (!fiber) return
+  if (!fiber) return;
   const domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
-  commitWork(fiber.child)
-  commitWork(fiber.sibling)
+  // fiber.dom在初始化时候是null
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom);
+  }
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
 }
 
 function render(element, container) {
@@ -46,15 +103,17 @@ function render(element, container) {
       children: [element],
     },
     // the fiber that we committed to the DOM in the previous commit phase
-    alternate: currentRoot
+    alternate: currentRoot,
   };
   nextUnitOfWork = wipRoot;
+  deletions = [];
 }
 
 let nextUnitOfWork = null;
 // last fiber tree we committed to the DOM
 let currentRoot = null;
 let wipRoot = null;
+let deletions = null;
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -69,20 +128,6 @@ function workLoop(deadline) {
 }
 
 requestIdleCallback(workLoop);
-
-function createDom(fiber) {
-  const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type);
-
-  const isProperty = key => key !== 'children';
-
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = fiber.props[name];
-    });
-
-  return dom;
-}
 
 /**
  * 执行work并返回下一个work
@@ -106,7 +151,7 @@ function performUnitOfWork(fiber) {
   // }
 
   const elements = fiber.props.children;
-  reconcileChildren(fiber, elements)
+  reconcileChildren(fiber, elements);
   /**
    * 返回下一个节点
    */
@@ -126,25 +171,62 @@ function reconcileChildren(wipFiber, elements) {
   /**
    * 根据fiber的子节点构造出一个新的fiber树
    */
-   let index = 0;
-   let prevSibling = null;
- 
-   while (index < elements.length) {
-     const element = elements[index];
-     const newFiber = {
-       type: element.type,
-       props: element.props,
-       parent: wipFiber,
-       dom: null,
-     };
-     if (index == 0) {
-       wipFiber.child = newFiber;
-     } else {
-       prevSibling.sibling = newFiber;
-     }
-     prevSibling = newFiber;
-     index += 1;
-   }
+  let index = 0;
+  /**
+   * wipFiber.alternate 首次运行指向null 后续执行指向上次commit时候的节点
+   * 获取旧的fiber节点
+   * The elements is the thing we want to render to the DOM and the oldFiber is what we rendered the last time.
+   */
+  let oldFiber = wipFiber.alternate?.child;
+  let prevSibling = null;
+  /**
+   * 遍历不能只针对新的元素，就的节点也应该做判断 这样才能知道旧节点是否该删除
+   */
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    // compare oldFiber to element
+    let newFiber = null;
+    const sameType = oldFiber && element && oldFiber.type === element.type;
+    if (sameType) {
+      // update
+      newFiber = {
+        type: oldFiber.type,
+        props: oldFiber.props,
+        parent: wipFiber,
+        dom: oldFiber.dom,
+        alternate: oldFiber,
+        effectTag: 'UPDATE',
+      };
+    }
+    if (element && !sameType) {
+      // add
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: wipFiber,
+        // 此处是reconcile阶段 不创建dom
+        dom: null,
+        // 新建的fiber alt是null
+        alternate: null,
+        effectTag: 'PLACEMENT',
+      };
+    }
+    if (oldFiber && !sameType) {
+      // delete
+      oldFiber.effectTag = 'DELETION';
+      deletions.push(oldFiber);
+    }
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+    if (index == 0) {
+      wipFiber.child = newFiber;
+    } else if(element) {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index += 1;
+  }
 }
 
 /** Didact 代替React */
@@ -165,7 +247,7 @@ const element = (
     <h2 style="text-align:right">from Didact</h2>
     <h2 style="text-align:right">from Didact</h2>
     <h2 style="text-align:right">from Didact</h2>
-    <h2 style="text-align:right">from Didact</h2>
+    <input onChange={evt => {console.log(evt.target.value)}}/>
   </div>
 );
 
